6074fc4fc1d70b9b4ffe2ce247a714466c9b7669
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-05 17:34:01 -0400

    working on render issues with updated react

--- airbnb_mockup1/src/index.js
+++ airbnb_mockup1/src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
+/* says render method no longer supported...
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+*/

f551eace9b303fe2533b21fd17b10f65978990b7
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-05 18:53:12 -0400

    save

--- airbnb_mockup1/src/index.js
+++ airbnb_mockup1/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
-
+/*
 /* says render method no longer supported...
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App tab="home" />);
 
-*/
+*/import { createRoot } from 'react-dom/client';
+ 
+
+ /*
+ createRoot.render(<App  />,document.getElementById('root'););
+ */

cd29507f8424ef294cea4b71381a2f2efd990a1f
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-05 17:00:17 -0400

    added main component and style

--- airbnb_mockup1/src/App.js
+++ airbnb_mockup1/src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import MainTop from"./components/MainTop"
 
 export default function App() {
     return (
         <>
          <Navbar    />
-        <h1>App component</h1>
+        <MainTop />
         </>
     )
 }
---
+++ airbnb_mockup1/src/components/MainTop.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MainTop(){
+    return(
+        <main>
+           <img src="../images/photo-grid.png" className="main--img"   />
+           <h1>Online Experiences</h1>
+           <p className="main--info">Join unique interactive activities lead by one-of-a-kind hostss--all without leaving home</p>
+        </main>
+    )
+}

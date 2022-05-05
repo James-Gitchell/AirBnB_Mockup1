49a92c85ddb7eaa76312761bd3efd8a08dc7d30d
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-05 14:37:08 -0400

    added react components and structure

---
+++ airbnb_mockup1/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App(){
+    return(
+         <>
+            <h1>This is the app component that will render the page</h1>
+         </>
+
+    )
+}
